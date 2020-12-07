be687b75d9655aff4e42eb79ade8ee0e74252fda
diseungie <73057618+diseungie@users.noreply.github.com>
2020-12-08 01:01:42 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+# ↓　passwordはMySQLをダウンロードするときに設定したやつ、他の人のパソコンでも同じPWで作動するかはよくわかんない
+# ↓　databaseは作成したdatabase（今は俺がtestdatabaseというdatabaseを作った状態）
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="632091",
+    database="testdatabase"
+    )
+
+mycursor = db.cursor()
+
+# ↓　""の中にMySQLの言語を書く（CREATE　DATABASEとか）、そしてRunする
+mycursor.execute("")
+
+
+# ↓　"DESCRIBE table名"でそのテーブルの現在情報を表示する
+# mycursor.execute("DESCRIBE Classes")
+#
+# ↓　"DESCRIBE table名"で表示されたテーブル情報をプリントする
+# for x in mycursor:
+#     print(x)
