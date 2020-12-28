14bd670ef66eaeb3cfd5a7b755d94e38a6b5c735
diseungie <73057618+diseungie@users.noreply.github.com>
2020-12-28 16:31:13 +0900

    installed required modules for Crawler.py in venv

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -12,12 +12,13 @@ db = mysql.connector.connect(
 mycursor = db.cursor()
 
 # ↓　""の中にMySQLの言語を書く（CREATE　DATABASEとか）、そしてRunする
-mycursor.execute("")
+# mycursor.execute("INSERT INTO SUBJECTS VALUES('全学共通教育科目','春夏学期','月1','スポーツ方法（春夏）Ⅰ','平賀　慧','日','○')")
+# mycursor.execute("INSERT INTO SUBJECTS VALUES('全学共通教育科目','春夏学期','月1','教養ゼミナール（小泉　順也）Ａ','小泉　順也','日','-')")
 
 
 # ↓　"DESCRIBE table名"でそのテーブルの現在情報を表示する
-# mycursor.execute("DESCRIBE Classes")
+mycursor.execute("SELECT * FROM SUBJECTS")
 
 # ↓　"DESCRIBE table名"で表示されたテーブル情報をプリントする
-# for x in mycursor:
-#     print(x)
+for x in mycursor:
+    print(x)
